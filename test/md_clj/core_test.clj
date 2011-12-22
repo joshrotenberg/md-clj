c50bbfc95633c98722f0bd16976c058c7c005640
josh rotenberg <joshrotenberg@gmail.com>
2011-12-21 16:05:33 -0800

    cleanup

--- test/md_clj/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns md-clj.core-test
-  (:use clojure.test
-        md-clj.core))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
--- test/md_clj/test/echo.clj
+++ test/md_clj/test/echo.clj
@@ -81,3 +81,4 @@
                            first
                            .getData
                            String.))))))))
+
